ae35c3b8bc2734c680596aff7af0251933a38e3c
Don <dmiesle@mac.com>
2018-10-24 08:04:30 -0400

    moved slideshow js to new file

--- js/app.js
+++ js/app.js
@@ -51,18 +51,3 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 };
-
-var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("myImages");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2000);
-};
